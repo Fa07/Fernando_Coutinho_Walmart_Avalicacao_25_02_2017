e2d30a734d6bdb33c0bc1c7b29666505edbcd81c
Fernando Coutinho <cout.fernando.a@gmail.com>
2017-02-26 18:05:15 -0300

    Novo cenario

--- features/step_definitions/validação_carrinho.rb
+++ features/step_definitions/validação_carrinho.rb
@@ -23,3 +23,28 @@ Então(/^o produto estará no carrinho$/) do
 	assert_text("Fralda Descartável Turma da Mônica Huggies Supreme Care Feminina G 32 Unidades")
 	sleep 3
 end
+
+Dado(/^que esteja na compra$/) do
+  visit ("https://www.walmart.com.br/smartphone-motorola-moto-g4-play-4g-dual-xt1600-preto-dual-chip-android-marshmallow-4g-memoria-de-16gb/4586763/pr")
+  click_link("Entre")
+  sleep 5
+end
+
+Dado(/^esteja efetuando o loggin$/) do
+  new_window = window_opened_by {}
+within_window new_window do
+  fill_in "signinField", :with => "35263646801"
+  fill_in "password", :with => "nandinho10"
+  click_button("signinButtonSend")
+  sleep 6
+end
+
+end
+
+Quando(/^finalizá\-lo$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então(/^comprar o produto com pagamento via boleto$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
--- features/validação_carrinho.feature
+++ features/validação_carrinho.feature
@@ -12,3 +12,11 @@ Funcionalidade: Adicionar produto no carrinho
 		Quando buscar o nome do produto
 		E solicitar sua compra
 		Então o produto estará no carrinho
+
+
+	@test	
+	Cenario: Finalizar a compra com login efetuado
+		Dado que esteja na compra
+		E esteja efetuando o loggin
+		Quando finalizá-lo
+		Então comprar o produto com pagamento via boleto
